578c5fa42703e2b67d1bb9644c847a26b36fd371
Rohit Rawat <ayushrawat220804@gmail.com>
2024-10-31 15:46:18 +0530

    Time: 3 ms (65.51%), Space: 9.6 MB (45.01%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -4,14 +4,12 @@ public:
         int len_s = s.length(), len_p = p.length();
         vector<vector<bool>> dp(len_s + 1, vector<bool>(len_p + 1, false));
         dp[0][0] = true; // An empty pattern matches an empty string
-
         // Initialize dp for patterns like a*, a*b*, a*b*c* (to match empty string)
         for (int j = 1; j <= len_p; ++j) {
             if (p[j - 1] == '*') {
                 dp[0][j] = dp[0][j - 2]; // '*' can match zero of the previous element
             }
         }
-
         // Fill the DP table
         for (int i = 1; i <= len_s; ++i) {
             for (int j = 1; j <= len_p; ++j) {
@@ -23,7 +21,6 @@ public:
                 }
             }
         }
-
         return dp[len_s][len_p]; // The result is in the bottom-right cell of the DP table
     }
 };
