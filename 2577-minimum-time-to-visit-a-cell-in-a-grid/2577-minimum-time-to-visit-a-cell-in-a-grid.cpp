602c5146ee8b729a87027c79a0da84d578b13be2
Rohit Rawat <ayushrawat220804@gmail.com>
2024-11-30 01:13:12 +0530

    Time: 417 ms (50.00%), Space: 71.9 MB (47.27%) - LeetHub

--- 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2577-minimum-time-to-visit-a-cell-in-a-grid/2577-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -1,55 +1,61 @@
+// thank you CodeWithMIK
 class Solution {
 public:
-    vector<vector<int>> direction{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     #define P pair<int, pair<int, int>>
-
+    vector<vector<int>> directions{{0,1},{0,-1},{1,0},{-1,0}};
+    
     int minimumTime(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
-        if (grid[0][1] > 1 && grid[1][0] > 1)
+        
+        // base case
+        if (grid[0][1] > 1 && grid[1][0] > 1){
             return -1;
+        }
         
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
-        priority_queue<P, vector<P>, greater<P>> pq;
-        
-        pq.push({grid[0][0], {0, 0}}); // Start at top-left corner
+        priority_queue<P , vector<P>, greater<P>> pq;
+        vector<vector<int>> result(m, vector<int>(n,INT_MAX));
+        pq.push({0,{0,0}});
         result[0][0] = 0;
-        while (!pq.empty()) {
-            // Get the current time, row, and column
+        vector<vector<int>> visited(m, vector<int> (n,false));
+        
+        while( !pq.empty()){
             int time = pq.top().first;
-            int row  = pq.top().second.first;
-            int col  = pq.top().second.second;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
             pq.pop();
             
-            // Reached destination
-            if (row == m - 1 && col == n - 1)
-                return result[m-1][n-1];
-            
-            // Mark the current cell as visited
-            if (visited[row][col]) continue;
+            if (row == m-1 && col == n-1 ){
+                return result [m-1][n-1]; // time value
+            }
+            if (visited[row][col] == true){
+                continue;
+            }
             visited[row][col] = true;
             
-            // Explore the neighboring cells
-            for (auto dir: direction) {
-                int r = row + dir[0];
-                int c = col + dir[1];
-                if (r < 0 || r >= m || c < 0 || c >= n || visited[r][c])
-                    continue;
+            for (auto &dir : directions){
+                int i = row + dir[0];
+                int j = col + dir[1];
                 
-                if (grid[r][c] <= time+1) {
-                    pq.push({time+1, {r, c}});
-                    result[r][c] = time+1;
-                } else if ((grid[r][c]-time)%2==0) {
-                    pq.push({grid[r][c]+1, {r, c}});
-                    result[r][c] = grid[r][c]+1;
-                } else {
-                    pq.push({grid[r][c], {r, c}});
-                    result[r][c] = grid[r][c];
+                if( i < 0 || i >= m || j < 0 || j>= n ){
+                    continue;
+                }
+                
+                if (grid[i][j] <= time + 1 ){
+                    pq.push({time + 1,{i,j}});
+                    result[i][j] = time + 1;
+                }
+                else if 
+                    ((grid[i][j] - time ) %2 == 0 ){ // code for even
+                    pq.push({grid[i][j] +1 , {i,j}});
+                    result[i][j] = grid[i][j] +1;
+                }
+                else{
+                    pq.push({grid[i][j] , {i,j}});
+                    result[i][j] = grid[i][j];
                 }
             }
         }
-        return -1;
+        return result[m-1][n-1];
     }
 };
