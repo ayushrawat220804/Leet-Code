436019692999187b4b8e99ac473ee4acd4017add
Rohit Rawat <ayushrawat220804@gmail.com>
2024-12-02 01:04:30 +0530

    Time: 3 ms (25.07%), Space: 13.6 MB (18.52%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
+    int binarysearch(vector<int> & arr, int target){
+        int l = 0;
+        int r = arr.size()-1;
+        
+        int mid;
+        while( l <= r ){
+            mid = l + (r-l)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if (arr[mid] > target){
+                r = mid-1;
+            }
+            else {
+                l = mid+1;
+            }
+        }            
+        return -1;
+    }
+    
     bool checkIfExist(vector<int>& arr) {
         int n = arr.size();
-        unordered_set<int> st;
-
-        for (int i = 0; i < n; i++) {
-            if (st.find(arr[i] * 2) != st.end()) {
-                return true;
-            } else if (arr[i]%2 == 0 && st.find(arr[i]/2) != st.end()){
+        sort(begin(arr), end(arr));
+        for (int i = 0; i < n; i++){
+            int j = binarysearch(arr, 2*arr[i]);
+            
+            if( j != -1 && j != i ){
                 return true;
             }
-            st.insert(arr[i]);
         }
         return false;
     }
