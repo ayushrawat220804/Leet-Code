39c5292c790b332ea639127b815813444539c02f
Rohit Rawat <ayushrawat220804@gmail.com>
2024-12-02 01:05:31 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (25.67%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,35 +1,17 @@
+// Thank You CodeWithMIK
+// method 1 : Brute force approach
 class Solution {
 public:
-    int binarysearch(vector<int> & arr, int target){
-        int l = 0;
-        int r = arr.size()-1;
-        
-        int mid;
-        while( l <= r ){
-            mid = l + (r-l)/2;
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if (arr[mid] > target){
-                r = mid-1;
-            }
-            else {
-                l = mid+1;
-            }
-        }            
-        return -1;
-    }
-    
     bool checkIfExist(vector<int>& arr) {
         int n = arr.size();
-        sort(begin(arr), end(arr));
         for (int i = 0; i < n; i++){
-            int j = binarysearch(arr, 2*arr[i]);
-            
-            if( j != -1 && j != i ){
-                return true;
+            for ( int j = 0; j < n; j++) {
+                if ( i != j && arr[i] == 2*arr[j]){
+                    return true;
+                }
             }
         }
+        
         return false;
     }
 };
