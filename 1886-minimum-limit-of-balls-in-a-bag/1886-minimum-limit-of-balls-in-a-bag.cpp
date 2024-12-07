a6047acf43233192dba4aaa2c68063fc181cdc7a
Rohit Rawat <ayushrawat220804@gmail.com>
2024-12-08 03:04:47 +0530

    Time: 29 ms (81.14%), Space: 59.7 MB (22.57%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,42 +1,13 @@
 class Solution {
 public:
-
-    bool isPossible(vector<int>& nums, int maxOps, int mid) {
-        int totalOps = 0; //to bring each number <= mid
-
-        for(int &num : nums) {
-            int ops = num / mid;
-
-            if(num % mid == 0) {
-                ops -= 1;
-            }
-
-            totalOps += ops;
-        }
-
-        return totalOps <= maxOps;
-    }
-
     int minimumSize(vector<int>& nums, int maxOperations) {
-        int l = 1;
-        int r = *max_element(begin(nums), end(nums));
-        
-        int result = r; //we have to minimize this
-
-        //O(n * log(MAX))
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-
-            if(isPossible(nums, maxOperations, mid) == true) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
-            }
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int ops = 0;
+            for (int n : nums) if ((ops += (n - 1) / mid) > maxOperations) break;
+            ops <= maxOperations ? high = mid : low = mid + 1;
         }
-
-        return result;
-
-
+        return high;
     }
 };
