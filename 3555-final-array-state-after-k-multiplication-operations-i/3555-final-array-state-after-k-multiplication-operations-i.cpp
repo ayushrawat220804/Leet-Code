dfbd0b45922e7a5c583df24f637c16159daed2f7
Rohit Rawat <ayushrawat220804@gmail.com>
2024-12-16 18:56:35 +0530

    Time: 0 ms (100%), Space: 27.6 MB (29.75%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        // Use a min-heap (priority queue) to keep track of the minimum element and its index
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        // Initialize the heap with values and indices
-        for (int i = 0; i < nums.size(); ++i) {
-            pq.push({nums[i], i});
+        int n = nums.size();
+
+        vector<pair<int, int>> vec(n);
+        for(int i = 0; i < n; i++){
+            vec[i] = {nums[i], i};
         }
-        while (k--) {   // Perform k operations
-            auto [val, idx] = pq.top(); // Get the smallest element
-            pq.pop();                   // Remove it from the heap
-            
-            nums[idx] = val * multiplier; // Update the array value
-            pq.push({nums[idx], idx});    // Push the updated value back to the heap
+
+        make_heap(begin(vec), end(vec), greater<>());
+
+        while(k--){
+            pop_heap(begin(vec), end(vec), greater<>());
+            pair<int, int> temp = vec.back();
+            vec.pop_back();
+
+            int idx = temp.second;
+            int number = temp.first;
+
+            nums[idx] = number*multiplier;
+
+            vec.push_back({nums[idx], idx});
+            push_heap(begin(vec), end(vec), greater<>());
         }
+
         return nums;
     }
 };
