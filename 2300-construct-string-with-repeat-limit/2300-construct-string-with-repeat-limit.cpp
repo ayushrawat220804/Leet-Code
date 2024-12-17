5a99b324089cf780dc284c2f1620b5878a6c001b
Rohit Rawat <ayushrawat220804@gmail.com>
2024-12-18 01:07:04 +0530

    Time: 14 ms (91.24%), Space: 28 MB (48.1%) - LeetHub

---
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string repeatLimitedString(string s, int repeatLimit) {
+        vector <int> count(26,0);
+        for(char &ch :s){
+            count[ch - 'a']++;
+        }
+        string result;
+
+        int i = 25;
+        while(i >= 0){
+            if(count[i] == 0){
+                i--;
+                continue;
+            }
+            char ch ='a' + i;
+            int frequency = min(count[i], repeatLimit);
+
+            result.append(frequency, ch);
+            count[i] = count[i] - frequency;
+
+            if(count[i] > 0){
+                int j = i - 1;
+                while(j >= 0 && count[j] == 0){
+                    j--;
+                }
+                if(j<0){
+                    break;
+                }
+                result.push_back('a' + j);
+                count[j]--;
+            }
+        }
+        return result;
+    }
+};
