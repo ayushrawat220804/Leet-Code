421a668c64b8d29a7442cd56c358df6e5fd7286b
Rohit Rawat <ayushrawat220804@gmail.com>
2025-01-01 23:33:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (23.35%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -4,14 +4,11 @@ public:
         int n = s.size();
         int countLeftZeros = 0; // Count of zeros on the left
         int countRightOnes = 0; // Count of ones on the right
-
         // Count all the '1's in the string for the initial right part
         for (char c : s) {
             if (c == '1') countRightOnes++;
         }
-
         int maxScore = 0;
-
         // Iterate through valid split points (1 to n-1)
         for (int i = 0; i < n - 1; ++i) {
             if (s[i] == '0') {
@@ -19,12 +16,9 @@ public:
             } else {
                 countRightOnes--;
             }
-
             // Calculate the score for the current split
             maxScore = max(maxScore, countLeftZeros + countRightOnes);
         }
-
         return maxScore;
     }
 };
-
