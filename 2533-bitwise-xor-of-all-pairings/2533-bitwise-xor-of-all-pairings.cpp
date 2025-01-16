77d1a59dfcde8b31e6b04094ce2226662ae804a9
Rohit Rawat <ayushrawat220804@gmail.com>
2025-01-17 01:00:18 +0530

    Time: 0 ms (100%), Space: 64.3 MB (64.63%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -1,33 +1,26 @@
-//Approach-1 (Using frequency count and Xor Property)
+//Approach-2 (Using Xor property)
 //T.C : O(n)
-//S.C : O(n)
+//S.C : O(1)
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
 
-        unordered_map<int, long> mp;
+        int XOR = 0;
 
-        for(int &num : nums1) {
-            mp[num] += n;
-        }
-
-
-        for(int &num : nums2) {
-            mp[num] += m;
-        }
-
-        int result = 0;
-        for(auto &it : mp) {
-            int num = it.first;
-            int freq = it.second;
-
-            if(freq%2 != 0) { //ODD
-                result ^= num;
+        if(m % 2 != 0) { //m is of odd length
+            for(int &num : nums2) {
+                XOR ^= num;
             }
         }
 
-        return result;
+        if(n % 2 != 0) { //n is of odd length
+            for(int &num : nums1) {
+                XOR ^= num;
+            }
+        }
+
+        return XOR;
     }
 };
