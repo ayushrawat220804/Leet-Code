c51830438ee4ef0e7a286319daa9d82c21dbd02f
Rohit Rawat <ayushrawat220804@gmail.com>
2025-02-02 23:43:30 +0530

    Time: 0 ms (100%), Space: 11 MB (85.59%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            vector<int> rotated(nums.begin() + i, nums.end());  // Rotate right part
-            rotated.insert(rotated.end(), nums.begin(), nums.begin() + i);  // Append left part
-            if (is_sorted(rotated.begin(), rotated.end()))  // Check if sorted
-            return true;
-        }
-        return false;
+        int count = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+            if (nums[i] > nums[(i + 1) % n])  // Compare with next (circular check)
+                count++;
+        return count <= 1;  // At most one descending pair
     }
 };
