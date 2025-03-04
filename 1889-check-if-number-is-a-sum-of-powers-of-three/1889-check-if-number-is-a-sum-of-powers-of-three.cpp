8ad9873238aaf8939b952babf74f9ddf9e2d849c
Rohit Rawat <ayushrawat220804@gmail.com>
2025-03-05 02:04:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (19.1%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        int ispowergreater = 0;
+        int ispowergreater = 0; 
         while ( pow(3, ispowergreater) <= n){
-            ispowergreater++;
+            ispowergreater++; // finds maxium power needed for n , till crosses n
         }
         while (n > 0 ){
             if (n >= pow(3,ispowergreater)){
-                n = n - pow(3,ispowergreater);
+                n = n - pow(3,ispowergreater); // make the power-- single times
             }
-            if ( n >= pow(3,ispowergreater)){
-                return false;
+            if ( n >= pow(3,ispowergreater)){ 
+                return false;  // if its still greater the needed n , return false;
+                // because we can't take power multiple times
+                // like 2^1, 2^2 is fine , but 2^1 then 2^1 is repeats wronge
             }
             ispowergreater--;
 
